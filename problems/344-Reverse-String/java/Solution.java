215d30033bcdd45d7a279848fb7e17cde88bd748
1nm <1nm@users.noreply.github.com>
2022-04-06 12:12:38 +0900

    Update solution for 344

--- problems/344-Reverse-String/java/Solution.java
+++ problems/344-Reverse-String/java/Solution.java
@@ -1,15 +1,12 @@
 public class Solution {
-    public String reverseString(String s) {
-        if (s == null || s.length() == 0) return s;
-        char[] array = s.toCharArray();
-        int i = 0, j = array.length - 1;
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
         while (i < j) {
-            char t = array[i];
-            array[i] = array[j];
-            array[j] = t;
-            i ++;
-            j --;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
-        return new String(array);
     }
 }
